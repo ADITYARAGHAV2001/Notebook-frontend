52adb395101b1073bf0025a0415250167e6a8435
ADITYARAGHAV2001 <aditya.it1@bvp.edu.in>
2023-08-18 00:10:32 +0530

    new_login

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,54 +1,75 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const LogIn = (props) => {
-  const [credentials, setCredentials] = useState({ email: "", password: "" })
+let Login = (props) => {
+  const [credentials, setCredentials] = useState({ email: '', password: '' });
   const navigate = useNavigate();
-  // const host = "https://notebook-backend-ydvu.onrender.com"
+  const host = "https://notebook-backend-ydvu.onrender.com"
   const handleSubmit = async (e) => {
-    try {
-      const response = await fetch("https://notebook-backend-ydvu.onrender.com/api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email: credentials.email, password: credentials.password })
-      });
-      const json = await response.json();
-      console.log(json);
-      if (json.success) {
-        localStorage.setItem('token', json.authtoken);
-        props.showAlert("Logged In Successfully", "success");
-        navigate('/');
-      } else {
-        props.showAlert("Invalid Credentials", "danger");
-      }
-    } catch (error) {
-      console.error("Error during fetch: ", error);
-      props.showAlert("Error connecting to the server.", "danger");
+    e.preventDefault();
+    const response = await fetch(`${host}/api/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: credentials.email, password: credentials.password }),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      // redirect
+      localStorage.setItem('token', json.authtoken);
+      navigate('/'); // Use navigate to go to the desired route
+      props.showAlert("Logged in","success")
+    } else {
+      props.showAlert("Invalid Credentials","danger")
+
     }
-
-
-  }
+  };
 
   const onChange = (e) => {
-    setCredentials({ ...credentials, [e.target.name]: e.target.value })
-  }
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id='email' name='email' value={credentials.email} onChange={onChange} aria-describedby="emailHelp" />
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            value={credentials.email}
+            onChange={onChange}
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id='password' name='password' value={credentials.password} onChange={onChange} />
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            value={credentials.password}
+            onChange={onChange}
+            name="password"
+            id="password"
+          />
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default LogIn
+export default Login;
